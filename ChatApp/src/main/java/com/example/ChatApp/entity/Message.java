857b9c2ef16c3a30bbc56bc3252a4a05014f9db0
Kannan.M.D <65282953+K-A-N-N-A-N@users.noreply.github.com>
2025-12-23 12:04:15 +0530

    Moved From H2 Db to MySql

--- ChatApp/src/main/java/com/example/ChatApp/entity/Message.java
+++ ChatApp/src/main/java/com/example/ChatApp/entity/Message.java
@@ -17,12 +17,15 @@ public class Message extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false, columnDefinition = "CHAR(36)")
     private String id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
     private ChatRoom chatRoom;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
     private ChatUser sender;
 
     @Enumerated(EnumType.STRING)
